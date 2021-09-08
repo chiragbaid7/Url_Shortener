4c3d924a4f4bfe5184936f727551ec910460987e
chiragbaid7 <chiragbaid7@gmail.com>
2021-09-09 00:14:14 +0530

    server.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const url = require("./api/public/url");
-const urlP = require("./api/private/url");
+const urlpublic = require("./api/public/url");
+const urlprivate = require("./api/private/url");
 const user = require("./api/public/user");
-const {verifytoken } = require("./auth/auth");
+const { verifytoken } = require("./auth/auth");
 const { limiter } = require("./helpers/rate-limit");
 const { ApiErrorHandeler, Api400Error } = require("./core/APIErrorHandeler");
-const { MONGO_URL, PORT } = require("./config/index");
+const { MONGO_URL } = require("./config/index");
 
 mongoose.connect(MONGO_URL);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(limiter);
 app.use(user);
-app.use(url);
-app.use("/api", verifytoken, urlP);
+app.use(urlpublic);
+app.use("/api", verifytoken, urlprivate);
 app.use(Api400Error); //Error for Invalid URL
 app.use(ApiErrorHandeler); //Error Handeler
 
-app.listen(PORT);
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,3 @@
+const { PORT } = require("./config/index");
+const app = require("./app");
+app.listen(PORT);
